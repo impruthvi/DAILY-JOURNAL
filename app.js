ac76cb780f0c5da1b394d134b4aa481314073f66
Pruthvi <pruthvirajput97@gmail.com>
2020-11-17 20:59:58 +0530

    install lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -49,6 +50,21 @@ app.post("/compose", function (req, res) {
 
   res.redirect("/");
 });
+
+
+app.get("/posts/:postName",function(req,res){
+  const  requestedTitle = _.lowerCase(req.params.postName);
+
+
+  posts.forEach(function(post){
+    const storeTitme = _.lowerCase(post.title);
+    if(requestedTitle === storeTitme){
+      console.log('Match found');
+    }else{
+      console.log('Match not found');
+    }
+  })
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
